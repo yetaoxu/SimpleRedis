447d823a820209ad815423187a8883108689d964
yetaoxu <yetaoxu@gmail.com>
2019-03-28 00:41:33 +0800

    dev: init

---
+++ cmake-build-debug/CMakeFiles/SimpleRedis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleRedis.dir/main.cpp.o"
+  "SimpleRedis.pdb"
+  "SimpleRedis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleRedis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
